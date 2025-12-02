6abd91b3121b2de14526e7572d29e44a1205824e
wrld777 <abejaoui@studenti.apuliadigitalmaker.it>
2025-12-02 11:14:34 +0100

    Initial commit

---
+++ src/test/java/com/Aereoporto/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aereoporto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
