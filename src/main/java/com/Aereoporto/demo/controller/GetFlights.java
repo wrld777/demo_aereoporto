fec97766c9f3193e1f363e9406a5a115abf19b4d
wrld777 <abejaoui@studenti.apuliadigitalmaker.it>
2025-12-02 16:11:59 +0100

    classi  model

--- src/main/java/com/Aereoporto/demo/controller/GetFlights.java
+++ src/main/java/com/Aereoporto/demo/controller/GetFlights.java
@@ -1,4 +1,5 @@
 package com.Aereoporto.demo.controller;
+
 import com.Aereoporto.demo.model.VoloOperativo;
 import com.Aereoporto.demo.repository.VoloOperativoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,6 +7,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
+@RestController
+@RequestMapping("/flights")
 public class GetFlights {
 
+    @Autowired
+    private VoloOperativoRepository voloOperativoRepository;
+
+    // GET /flights/{flightId}
+    @GetMapping("/{flightId}")
+    public ResponseEntity<VoloOperativo> getFlightsById(@PathVariable String flightId){
+
+        Optional<VoloOperativo> op = voloOperativoRepository.findById(flightId);
+
+        return op.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/Aereoporto/demo/controller/GetOperationalFlights.java
+++ src/main/java/com/Aereoporto/demo/controller/GetOperationalFlights.java
@@ -1,4 +1,23 @@
 package com.Aereoporto.demo.controller;
 
+import com.Aereoporto.demo.model.VoloOperativo;
+import com.Aereoporto.demo.repository.VoloOperativoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
 public class GetOperationalFlights {
+
+    @Autowired
+    private VoloOperativoRepository voloOperativoRepository;
+
+    @GetMapping("/operational_flights")
+    public ResponseEntity<List<VoloOperativo>> getAllOperationalFlights() {
+        List<VoloOperativo> list = voloOperativoRepository.findAll();
+        return ResponseEntity.ok(list);
+    }
 }
